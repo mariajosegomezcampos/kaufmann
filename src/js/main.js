bfaca5df8feb1cd1fa4380ff3abaeff6d2aafab0
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-08-03 18:10:43 -0400

    terminado ultimos cambios

--- src/js/main.js
+++ src/js/main.js
@@ -23,24 +23,22 @@ window.onload = () =>{
 
     })
 
-    .catch(function(err) {
-        console.error(err);
+    .catch(function(err) {console.error(err);
     });
 }
 //funcion buscador
 //Boton buscar 
- const search= document.getElementById("submit-buscador");
+ const search = document.querySelector('#submit-buscador');
  search.addEventListener("click", () =>{
    
      const namesearch=document.getElementById("buscador").value;
-    fetch('https://api.mercadolibre.com/sites/MLC/search?q='+namesearch)
-   .then(resp => resp.json() )
+    fetch('https://api.mercadolibre.com/sites/MLC/search?q='+ namesearch)
+   .then(response => response.json() )
     .then(data => {
-          
-         data.results.forEach (e => {
-           let show=document.getElementById("filterList");
+         data.results.forEach(e => {
+           let show = document.getElementById("filterList");
            show.innerHTML = " ";
-            show.innerHTML +=  `<div class="repuesto">
+            show.innerHTML += `<div class="repuesto">
            <img class="imagen-repuesto" src="${e.thumbnail}" alt="${e.title}"> 
            <div class="info-repuesto">
            <h4>${e.title}</h4>
@@ -50,10 +48,12 @@ window.onload = () =>{
                 </div>
                <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>       
                      </div>
-     </div>` 
-           })    
+     </div>` ;
+           })   
          })
-
+         .catch(function(err) {
+            console.error(err);
+        });
  });
 // funcion filtrar
 
@@ -61,32 +61,34 @@ window.onload = () =>{
     const filter = document.getElementById('filter');
     filter.addEventListener('change',() => {   
     const productSelect = filter[filter.selectedIndex].value;
-    console.log(productSelect); 
+    console.log(productSelect.lenth)
+   
     fetch('https://api.mercadolibre.com/sites/MLC/search?q=$' + productSelect )
     .then(resp => resp.json() )
     .then(data => {
-        
          let listCategory= document.getElementById('filterList')
-         data.results.map(e =>{
-           console.log(data)
+         console.log(data.results)
+         data.results.forEach((e) =>{
            listCategory.innerHTML ='';
-            listCategory.innerHTM += `
-                <div class="repuesto">
-                <img class="imagen-repuesto" src="${e.thumbnail}" alt="img"> 
-                <div class="info-repuesto">
-                <h4>${e.title}</h4>
-                <img src="./img/estrellas.png" alt="">
-                    <div class="precio">
-                    <p>$ ${e.price}</p>
-                    </div>
-                    <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>
-                </div>
-            </div>
-           
-               
-                 `;
+            listCategory.innerHTML += `<div class="repuesto">
+            <img class="imagen-repuesto" src="${e.thumbnail}" alt=""> 
+            <div class="info-repuesto">
+            <h4>${e.title}</h4>
+             <img src="./img/estrellas.png" alt="">
+              <div class="precio">
+                 <p>$ ${e.price}</p>
+                 </div>
+                <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>       
+                      </div>
+      </div>`; 
         })
         
     })
 })
-      
+  
+const returnhome= document.getElementById("return");
+returnhome.addEventListener('click',()=>{
+
+   location.reload();
+
+});
