44a9712aabd9d2d000e3a6101c8e6c66089fc048
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-08-03 20:31:11 -0400

    reslviendo error al traer la data

--- src/js/main.js
+++ src/js/main.js
@@ -35,53 +35,54 @@ window.onload = () =>{
     fetch('https://api.mercadolibre.com/sites/MLC/search?q='+ namesearch)
    .then(response => response.json() )
     .then(data => {
-         data.results.forEach(e => {
+        const data_a = data.results;
            let show = document.getElementById("filterList");
-           show.innerHTML = " ";
-            show.innerHTML += `<div class="repuesto">
-           <img class="imagen-repuesto" src="${e.thumbnail}" alt="${e.title}"> 
+            show.innerHTML = data_a.map( element =>{
+            return `<div class="repuesto">
+           <img class="imagen-repuesto" src="${element.thumbnail}" alt="${element.title}"> 
            <div class="info-repuesto">
-           <h4>${e.title}</h4>
+           <h4>${element.title}</h4>
             <img src="./img/estrellas.png" alt="">
              <div class="precio">
-                <p>$ ${e.price}</p>
+                <p>$ ${element.price}</p>
                 </div>
                <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>       
                      </div>
      </div>` ;
            })   
-         })
-         .catch(function(err) {
-            console.error(err);
+        
+         
         });
- });
+    });
+
 // funcion filtrar
 
 
     const filter = document.getElementById('filter');
     filter.addEventListener('change',() => {   
     const productSelect = filter[filter.selectedIndex].value;
-    console.log(productSelect.lenth)
+    
    
     fetch('https://api.mercadolibre.com/sites/MLC/search?q=$' + productSelect )
     .then(resp => resp.json() )
     .then(data => {
+
          let listCategory= document.getElementById('filterList')
-         console.log(data.results)
-         data.results.forEach((e) =>{
-           listCategory.innerHTML ='';
-            listCategory.innerHTML += `<div class="repuesto">
-            <img class="imagen-repuesto" src="${e.thumbnail}" alt=""> 
-            <div class="info-repuesto">
-            <h4>${e.title}</h4>
-             <img src="./img/estrellas.png" alt="">
-              <div class="precio">
-                 <p>$ ${e.price}</p>
-                 </div>
-                <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>       
-                      </div>
-      </div>`; 
-        })
+        const data_a = data.results;
+        
+                    listCategory.innerHTML = data_a.map( element =>{
+                    return`<div class="repuesto">
+                    <img class="imagen-repuesto" src="${element.thumbnail}" alt=""> 
+                    <div class="info-repuesto">
+                    <h4>${element.title}</h4>
+                     <img src="./img/estrellas.png" alt="">
+                       <div class="precio">
+                      <p>$ ${element.price}</p>
+                         </div>
+                        <a href="#" class="boton" data-id="1">Agregar Al Carrito</a>       
+                           </div>
+              </div>`; 
+         })
         
     })
 })
